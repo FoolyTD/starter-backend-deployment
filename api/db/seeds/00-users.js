0cabdfc73b04fee673c4842ff3a69df78c986635
FoolyTD <trcariaga@utexas.edu>
2021-07-13 19:24:17 -0500

    changed index.js to app.js in hopes of fixing server issues

--- api/db/seeds/00-users.js
+++
@@ -1,9 +0,0 @@
-const users = require("../fixtures/users");
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex
-    .raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
-    .then(function () {
-      return knex("users").insert(users);
-    });
-};
